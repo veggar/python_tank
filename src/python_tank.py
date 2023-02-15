8741e651a9638eb8cb996b30c914d36fee6116b6
veggar <veggar.dev@gmail.com>
2023-02-15 17:48:24 +0900

    prepare display setting for rendering object.

--- src/python_tank.py
+++ src/python_tank.py
@@ -1,12 +1,24 @@
-import pygame
+import sys
+import signal
 import os
+
+import pygame
 import game_main as game
+from .display import display
 
 # define a main function
 def main():
     # get path for loading resource from main file
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
+    # Check if running on pi
+    from display.pygamePixelDisplay import PygamePixelDisplay
+    display = PygamePixelDisplay(caption="PiTable Emulator",
+                                    gridSize=[16, 16],
+                                    width=1000,
+                                    height=1000,
+                                    space=2)
+        
     # initialize the pygame module
     pygame.init()
     
@@ -21,9 +33,10 @@ def main():
     # define a variable to control the main loop
     running = True
     
-    # main loop
+    # for initialize
     game.init()
-    
+
+    # main loop
     while running:
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
@@ -32,7 +45,15 @@ def main():
             elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
                 running = game.process(event)
     
-    
+    # for exit
+    display.fill([0, 0, 0])
+    display.show()
+
+    # Quit game and close window
+    game.quit()
+    pygame.quit()
+    sys.exit(0)
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
