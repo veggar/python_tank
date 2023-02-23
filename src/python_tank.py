9f1e4daa6c075f6edd30c82a359b58909206d033
veggar <veggar.dev@gmail.com>
2023-02-23 16:41:22 +0900

    Apply game loop with PiTable.

--- src/python_tank.py
+++ src/python_tank.py
@@ -4,20 +4,12 @@ import os
 
 import pygame
 import game_main as game
-from .display import display
+# import display.display as display
 
 # define a main function
 def main():
     # get path for loading resource from main file
     dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    # Check if running on pi
-    from display.pygamePixelDisplay import PygamePixelDisplay
-    display = PygamePixelDisplay(caption="PiTable Emulator",
-                                    gridSize=[16, 16],
-                                    width=1000,
-                                    height=1000,
-                                    space=2)
         
     # initialize the pygame module
     pygame.init()
@@ -25,11 +17,17 @@ def main():
     # load and set the logo
     logo = pygame.image.load(dir_path+"/../resource/logo32x32.png")
     pygame.display.set_icon(logo)
-    pygame.display.set_caption("pytank")
     
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((480,320))
-    
+    from display.pygamePixelDisplay import PygamePixelDisplay
+    display = PygamePixelDisplay(caption="PyTank",
+                                    gridSize=[2, 2],
+                                    width=480,
+                                    height=320,
+                                    space=2)
+
+    display.init()
+
     # define a variable to control the main loop
     running = True
     
@@ -44,11 +42,12 @@ def main():
                 running = False
             elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
                 running = game.process(event)
-    
-    # for exit
-    display.fill([0, 0, 0])
-    display.show()
+        
+        display.fill([0, 0, 0])
+        game.render(display)
+        display.show()
 
+    # for exit
     # Quit game and close window
     game.quit()
     pygame.quit()
