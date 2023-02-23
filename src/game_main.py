7100e73bc3f61b79b558f82e07882255922c1c06
veggar <veggar.dev@gmail.com>
2023-02-23 16:44:31 +0900

    Add files.

---
+++ src/game_main.py
@@ -0,0 +1,49 @@
+import time
+
+import pygame
+import game_object
+
+
+# reference for https://github.com/nicostudt/PiTable/blob/b73681c1508b8099713252a47f60f06d04e7300a/engine/engine.py
+
+Tanks = []
+
+def init():
+    Tanks.append(game_object.Tank("tank 1", 50, 50, pygame.Color(0, 0, 255)))
+    return
+
+def process(event, inverval_ms=33):
+
+    if event != None and event.type == pygame.KEYDOWN:
+        
+        print("event {}:{}".format(event.type, event.key))
+
+        match int(event.key):
+            case pygame.K_q: # change the value to False, to exit the main loop
+               return False
+            case pygame.K_d:
+                render(None)
+            case pygame.K_UP:
+                Tanks[0].addPosition(-5,-5)
+            case pygame.K_DOWN:
+                Tanks[0].addPosition(5,5)
+            case pygame.K_LEFT:
+                Tanks[0].addAngle(-0.1)
+            case pygame.K_RIGHT:
+                Tanks[0].addAngle(0.1)
+            case _:
+                pass
+                
+
+    return True
+
+def control_input(key):
+    return
+
+def render(display):
+    if len(Tanks) > 0:
+        print(Tanks[0])
+    return
+
+def quit():
+    return
---
+++ src/game_object.py
@@ -0,0 +1,77 @@
+import pygame
+import math
+
+DEFAULT_ENERGY:int = 100
+DEFAULT_SIZE:int = 40
+
+Bullets = []
+
+class Tank:
+    def __init__(self, name, cx=0, cy=0, color=pygame.Color(255,255,255)):
+        self.reset(name, cx, cy, color)
+
+    def reset(self, name, cx, cy, color):
+
+        self.name = name
+        self.color = color
+        
+        self.size = DEFAULT_SIZE
+
+        self.setPosition(cx,cy)        
+        self.setAngle(0)
+
+        self.setEnergy(DEFAULT_ENERGY)
+        self.delete = False
+
+    def setPosition(self, x, y):
+        # self.rect = pygame.Rect(x-self.size/2, y-self.size/2, x+self.size/2, y+self.size/2)
+        self.cx = x
+        self.cy = y
+
+    def addPosition(self, x, y):
+        self.cx += x * math.sin(self.angle)
+        self.cy -= y * math.cos(self.angle)
+
+    def setAngle(self, angle):
+        self.angle = angle
+
+    def addAngle(self, angle):
+        self.angle += angle
+
+    def setEnergy(self, energy):
+        self.energy = energy
+
+    def __str__(self):
+        return "cx: {}, cy: {}, angle: {}, energy: {}, state: {}".format(self.cx, self.cy, self.angle, self.energy, self.delete)
+
+
+class Bullet:
+    def __init__(self, owner:Tank):
+        self.reset(owner)
+
+    def reset(self, owner:Tank):
+        self.owner = owner
+        self.color = owner.color
+        self.x = owner.cx+20
+        self.y = owner.cy+40
+        self.angle = owner.angle
+        self.delete = False
+
+    def addPosition(self, x, y):
+        self.x -= x * math.sin(self.angle)
+        self.y += y * math.cos(self.angle)
+
+    def __str__(self):
+        return "x: {}, y:{}, angle: {}, owner: {}, state: {}".format(self.x, self.y, self.angle, self.owner.name, self.delete)
+
+def createBullet(self, tank:Tank):
+    for bullet in Bullets:
+        if bullet.delete:
+            bullet.reset(tank)
+            return
+
+    bullet = Bullet(tank)
+    Bullets.append(bullet)
+
+    print("create bullet of {}, length: {}".format(bullet.owner.name, len(Bullets)))
+
